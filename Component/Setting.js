ae915d63fb3b9961e87b249fc492965ff9fbb25b
adhermit <maharjanraman456@gmail.com>
2024-04-22 13:04:08 +0200

    sharad condo

--- Component/Setting.js
+++
@@ -1,9 +0,0 @@
-import {View, Text} from 'react-native';
-
-export default function SettingScreen() {
-    return (
-        <View style={{ flex:1, alignItems: 'center', JustifyContent:'center'}}>
-            <Text>Settings Screen</Text>
-        </View>
-    );
-}
