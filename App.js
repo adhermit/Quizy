4af4c1708b9af890c7f03c9787dd0661f2c64b33
adhermit <maharjanraman456@gmail.com>
2024-04-22 14:36:28 +0200

    update

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-
+import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-
-
-
+import SettingsScreen from './components/Setting';
 import HomeScreen from "./components/Home";
-
+import CategoryScreen from "./components/Categories";
+import ScoreScreen from './components/ScoreBoard';
 
 
 const Tab = createBottomTabNavigator();
@@ -29,9 +28,38 @@ export default function App() {
             ),
           }}
         />
-        
-      <StatusBar style="auto" />
+          <Tab.Screen
+            name="Category"
+            component={CategoryScreen}
+            options={{
+              tabBarLabel: 'Categories',
+              tabBarIcon: ({ color, size }) => (
+                <Icon name="list" size={size} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Score Board"
+            component={ScoreScreen}
+            options={{
+              tabBarLabel: 'Scoreboard',
+              tabBarIcon: ({ color, size }) => (
+                <Icon name="score" size={size} color={color} />
+              ),
+            }}
+          />
+        <Tab.Screen
+          name="Settings"
+          component={SettingsScreen}
+          options={{
+            tabBarLabel: 'Settings',
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="cog" size={size} color={color} />
+            ),
+          }}
+        />
       </Tab.Navigator>
+      <StatusBar style="auto" />
     </NavigationContainer>
   );
 }
---
+++ components/Categories.js
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native';
+
+export default function CategoryScreen() {
+    return (
+        <View style={{ flex:1, alignItems: 'center', JustifyContent:'center'}}>
+            <Text>Categories</Text>
+        </View>
+    );
+}
