4d5c6af34a79dc89c99acc3f96c04b2a4e831313
sharad68 <sharadbhusal49@gmail.com>
2024-04-23 16:36:36 +0200

    fjsdgfjsdglu

--- components/Categories.js
+++ components/Categories.js
@@ -1,17 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, Button } from 'react-native';
-import { categoriesData } from '../Data/Category';
+import React, { useState, useEffect } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { categoriesData } from "../Data/Category";
+import { List } from "react-native-paper";
 
 export default function CategoryScreen() {
-    
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Categories</Text>
-            {categoriesData.map((category, index) => (
-                <View key={index}>
-                    <Text>{category.name}</Text>
-                </View>
-            ))}
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>Categories</Text>
+      <List.Section>
+        {categoriesData.map((category, index) => (
+          <List.Item title={category.name} left={(props) => <List.Icon {...props} icon={category.icon} />} />
+        ))}
+      </List.Section>
+    </View>
+  );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
