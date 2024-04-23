e12784e362c9a222c653e3d3bce9588b407f8d03
sharad68 <sharadbhusal49@gmail.com>
2024-04-23 16:16:50 +0200

    bdsjkvgjsd

--- components/Categories.js
+++ components/Categories.js
@@ -1,37 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, Button } from 'react-native';
-import { categoriesData } from './Data/Category';
+import { categoriesData } from '../Data/Category';
 
 export default function CategoryScreen() {
-    const [categories, setCategories] = useState([]);
-
-    useEffect(() => {
-        setCategories(categoriesData);
-    }, []);
-
-    const fetchData = () => {
-        setCategories(categoriesData);
-    };
-
+    
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Categories</Text>
-            {categories.map((category, index) => (
+            {categoriesData.map((category, index) => (
                 <View key={index}>
                     <Text>{category.name}</Text>
                 </View>
             ))}
-            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                {categories.length > 0 ? (
-                    <View>
-                        <Text>Data loaded successfully:</Text>
-                        <Text>{JSON.stringify(categories)}</Text>
-                    </View>
-                ) : (
-                    <Text>Loading data...</Text>
-                )}
-                <Button title="Refresh Data" onPress={fetchData} />
-            </View>
         </View>
     );
 }
