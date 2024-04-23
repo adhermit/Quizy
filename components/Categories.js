ae8b08b88a66baf65a592bfec9a72a57c96c1278
adhermit <maharjanraman456@gmail.com>
2024-04-23 14:03:45 +0200

    button update

--- components/Categories.js
+++ components/Categories.js
@@ -1,9 +1,12 @@
 import {View, Text} from 'react-native';
+import {questionsData} from 'Questions';
 
 export default function CategoryScreen() {
     return (
         <View style={{ flex:1, alignItems: 'center', JustifyContent:'center'}}>
             <Text>Categories</Text>
+            {questionsData}
         </View>
     );
 }
+
