bd59dd9c1476e6d5bbe21cf48055b9da6f33bbb6
sharad68 <sharadbhusal49@gmail.com>
2024-04-23 15:55:18 +0200

    works

--- components/Categories.js
+++ components/Categories.js
@@ -1,11 +1,9 @@
 import {View, Text} from 'react-native';
-import {questionsData} from 'Questions';
 
 export default function CategoryScreen() {
     return (
         <View style={{ flex:1, alignItems: 'center', JustifyContent:'center'}}>
             <Text>Categories</Text>
-            {questionsData}
         </View>
     );
 }
--- components/Home.js
+++ components/Home.js
@@ -1,10 +1,13 @@
 import { StyleSheet, View, Image } from "react-native";
-
+import { Button } from "react-native-paper";
 
 export default function HomeScreen ({navigation}) {
 return (
     <View style={{ flex:1,alignItems:"center",justifyContent:"center", backgroundColor: '#d46de5'}}>
         <Image source = {require("./../assets/Image/logo-quizy.png")} style = {styles.homeImage}/>
+        <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+    Press me
+  </Button>
     </View>
 );
 }
