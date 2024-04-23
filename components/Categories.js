106ffa5b90b00e9b36bd82e827fb3c0faea2efcd
adhermit <maharjanraman456@gmail.com>
2024-04-23 16:46:39 +0200

    update

--- components/Categories.js
+++ components/Categories.js
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet, ScrollView } from "react-native";
 import { categoriesData } from "../Data/Category";
 import { List } from "react-native-paper";
 
 export default function CategoryScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Categories</Text>
-      <List.Section>
-        {categoriesData.map((category, index) => (
-          <List.Item title={category.name} left={(props) => <List.Icon {...props} icon={category.icon} />} />
-        ))}
-      </List.Section>
-    </View>
+    <ScrollView>
+        <View style={styles.container}>
+        <Text>Categories</Text>
+        <List.Section>
+            {categoriesData.map((category, index) => (
+            <List.Item title={category.name} left={(props) => <List.Icon {...props} icon={category.icon} />} />
+            ))}
+        </List.Section>
+        </View>
+    </ScrollView>
   );
 }
 const styles = StyleSheet.create({
