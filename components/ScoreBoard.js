9e59dd0864403098e1da3d0c25fe289ec863f051
adhermit <maharjanraman456@gmail.com>
2024-04-24 15:59:50 +0200

    update

--- components/ScoreBoard.js
+++ components/ScoreBoard.js
@@ -3,7 +3,7 @@ import {View, Text} from 'react-native';
 export default function ScoreScreen() {
     return (
         <View style={{ flex:1, alignItems: 'center', JustifyContent:'center'}}>
-            <Text>Categories</Text>
+            <Text>Scoreboard</Text>
         </View>
     );
 }
