90b0da4363bba74c36743799e49e690406362a20
sharad68 <sharadbhusal49@gmail.com>
2024-04-24 17:43:25 +0200

    bjdsbfjkds

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {PaperProvider} from 'react-native-paper';
 import SettingsScreen from './components/Setting';
 import HomeScreen from "./components/Home";
 import CategoryScreen from "./components/Categories";
 import ScoreScreen from './components/ScoreBoard';
+import quizScreen from './components/Quiz';
+
 
 
 
 const Tab = createBottomTabNavigator();
+
+const Stack = createStackNavigator();
 
 export default function App() {
  
@@ -20,6 +26,7 @@ export default function App() {
   return (
     <PaperProvider>
     <NavigationContainer>
+    
       <Tab.Navigator>
         <Tab.Screen
           name="Home"
@@ -63,6 +70,10 @@ export default function App() {
         />
       </Tab.Navigator>
       <StatusBar style="auto" />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Quiz" component={quizScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
     </PaperProvider>
   );
--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,13 @@
 import { StyleSheet, View, Image, Alert } from "react-native";
 import { Button } from "react-native-paper";
 
-export default function HomeScreen () {
+export default function HomeScreen ({navigation}) {
 return (
     <View style={{ flex:1,alignItems:"center",justifyContent:"center", backgroundColor: '#d46de5'}}>
         <Image source = {require("./../assets/Image/logo-quizy.png")} style = {styles.homeImage}/>
-        <Button icon="camera" mode="contained" onPress={() => alert('Pressed')}>
+        <Button  mode="contained" onPress={() => navigation.navigate('Quiz')}>
     Press me
+        
   </Button>
     </View>
 );
@@ -16,5 +17,6 @@ const styles = StyleSheet.create({
         "height": 200,
         resizeMode: 'contain',
         marginBottom: 20,
-    }
+    },
 });
+
