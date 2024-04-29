9fe6fedd8b0e66c12ff7670b30b15194b587205b
sharad68 <sharadbhusal49@gmail.com>
2024-04-29 15:48:46 +0200

    asdfghjkjkl

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,22 +1,63 @@
-import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
-
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Button, ActivityIndicator } from 'react-native';
+import ListCard from "./ListCard"
 
 export default function QuizScreen() {
+  const [quiz, setQuiz] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const response = await fetch("https://opentdb.com/api.php?amount=10&encode=base64");
+      const result = await response.json();
+      setQuiz(result.results);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setError("An error occurred while fetching data.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleNext = () => {
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+  };
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" color="blue" />
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={styles.container}>
+        <Text>Error: {error}</Text>
+      </View>
+    );
+  }
+
   return (
-   
-    <View style={style.container}>
-      <Text>Details Screen</Text>
+    <View style={styles.container}>
+      {quiz.length > 0 && quiz[currentQuestionIndex] && (
+        <ListCard quiz={quiz[currentQuestionIndex]} onNext={handleNext} />
+      )}
     </View>
-    
   );
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
-
